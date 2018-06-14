fb6511c791fe11290d2557e658dfbe736c713dee
Jairo Nino <jaironino@Jairos-MacBook-Air.local>
2018-06-14 13:35:42 -0500

    Second Commit

---
+++ src/variables/ManejoVariablesTest.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package variables;
+
+/**
+ *
+ * @author jaironino
+ */
+public class ManejoVariablesTest {
+    
+    public static void main(String args[]){
+        
+        boolean bool1;
+        int casa;
+    
+        casa = 10;
+        bool1 = true;
+    
+        boolean bool2 = false;
+    
+        System.out.println("Valor bool1 =" +bool1);
+    
+    
+    }
+    
+    
+    
+}
